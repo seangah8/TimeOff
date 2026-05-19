fe170972197a6150989c2c67c31f2d86d6376a5b
seangah8 <seangah8@gmail.com>
2026-05-19 22:34:02 +0300

    feat: configure Axios instance with 401 interceptor

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import router from '@/router';
+
+const api = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  withCredentials: true,
+});
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      router.push('/login');
+    }
+    return Promise.reject(error);
+  },
+);
+
+export default api;
